d5adb3f50af3db8efd638bea8396ffa056bf3c71
anton <anton@LAPTOP-VES5DEI7>
2021-11-28 14:20:01 -0700

    Agregar archivos de proyecto.

---
+++ Practica1-Sist. Sensado y Monitoreo/Form1.Designer.cs
@@ -0,0 +1,334 @@
+﻿namespace Practica1_Sist.Sensado_y_Monitoreo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tbDatos = new System.Windows.Forms.TextBox();
+            this.tbTemp = new System.Windows.Forms.TextBox();
+            this.tbDist = new System.Windows.Forms.TextBox();
+            this.tbH = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cbPuertos = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btConectar = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbAct = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbDatos
+            // 
+            this.tbDatos.Font = new System.Drawing.Font("Arial Rounded MT Bold", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbDatos.Location = new System.Drawing.Point(221, 69);
+            this.tbDatos.Margin = new System.Windows.Forms.Padding(6);
+            this.tbDatos.Name = "tbDatos";
+            this.tbDatos.Size = new System.Drawing.Size(295, 34);
+            this.tbDatos.TabIndex = 0;
+            // 
+            // tbTemp
+            // 
+            this.tbTemp.Font = new System.Drawing.Font("Arial Rounded MT Bold", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbTemp.Location = new System.Drawing.Point(168, 79);
+            this.tbTemp.Margin = new System.Windows.Forms.Padding(6);
+            this.tbTemp.Name = "tbTemp";
+            this.tbTemp.Size = new System.Drawing.Size(128, 34);
+            this.tbTemp.TabIndex = 1;
+            // 
+            // tbDist
+            // 
+            this.tbDist.Font = new System.Drawing.Font("Arial Rounded MT Bold", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbDist.Location = new System.Drawing.Point(168, 125);
+            this.tbDist.Margin = new System.Windows.Forms.Padding(6);
+            this.tbDist.Name = "tbDist";
+            this.tbDist.Size = new System.Drawing.Size(128, 34);
+            this.tbDist.TabIndex = 2;
+            // 
+            // tbH
+            // 
+            this.tbH.Font = new System.Drawing.Font("Arial Rounded MT Bold", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbH.Location = new System.Drawing.Point(168, 33);
+            this.tbH.Margin = new System.Windows.Forms.Padding(6);
+            this.tbH.Name = "tbH";
+            this.tbH.Size = new System.Drawing.Size(128, 34);
+            this.tbH.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 69);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(163, 23);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Datos del Serial";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 33);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 23);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Temperatura";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 79);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 23);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Distancia";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 125);
+            this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(105, 23);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Humedad";
+            // 
+            // cbPuertos
+            // 
+            this.cbPuertos.Font = new System.Drawing.Font("Arial Rounded MT Bold", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbPuertos.FormattingEnabled = true;
+            this.cbPuertos.Location = new System.Drawing.Point(135, 19);
+            this.cbPuertos.Margin = new System.Windows.Forms.Padding(6);
+            this.cbPuertos.Name = "cbPuertos";
+            this.cbPuertos.Size = new System.Drawing.Size(189, 35);
+            this.cbPuertos.TabIndex = 10;
+            this.cbPuertos.DropDown += new System.EventHandler(this.cmbPuertos_DropDown);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(18, 19);
+            this.label6.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(75, 23);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Puerto";
+            // 
+            // btConectar
+            // 
+            this.btConectar.Location = new System.Drawing.Point(366, 19);
+            this.btConectar.Margin = new System.Windows.Forms.Padding(6);
+            this.btConectar.Name = "btConectar";
+            this.btConectar.Size = new System.Drawing.Size(150, 40);
+            this.btConectar.TabIndex = 12;
+            this.btConectar.Text = "Conectar";
+            this.btConectar.UseVisualStyleBackColor = true;
+            this.btConectar.Click += new System.EventHandler(this.btConectar_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(333, 33);
+            this.label8.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(33, 23);
+            this.label8.TabIndex = 20;
+            this.label8.Text = "°C";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(333, 125);
+            this.label9.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(41, 23);
+            this.label9.TabIndex = 21;
+            this.label9.Text = "(%)";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(333, 79);
+            this.label10.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(28, 23);
+            this.label10.TabIndex = 22;
+            this.label10.Text = "m";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(49, 38);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(111, 23);
+            this.label5.TabIndex = 23;
+            this.label5.Text = "Ventilador";
+            // 
+            // tbAct
+            // 
+            this.tbAct.Location = new System.Drawing.Point(8, 69);
+            this.tbAct.Name = "tbAct";
+            this.tbAct.Size = new System.Drawing.Size(196, 31);
+            this.tbAct.TabIndex = 24;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.White;
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.tbTemp);
+            this.groupBox1.Controls.Add(this.tbDist);
+            this.groupBox1.Controls.Add(this.tbH);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Location = new System.Drawing.Point(18, 119);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(387, 172);
+            this.groupBox1.TabIndex = 28;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "SENSORES";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(49, 108);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(111, 23);
+            this.label7.TabIndex = 29;
+            this.label7.Text = "Ventilador";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 139);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(196, 31);
+            this.textBox1.TabIndex = 30;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.tbAct);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Location = new System.Drawing.Point(411, 119);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(210, 181);
+            this.groupBox2.TabIndex = 31;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(657, 12);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(530, 300);
+            this.chart1.TabIndex = 32;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1199, 314);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btConectar);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.cbPuertos);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbDatos);
+            this.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.Text = "PRACTICA 1: UV Y BMP";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbDatos;
+        private System.Windows.Forms.TextBox tbTemp;
+        private System.Windows.Forms.TextBox tbDist;
+        private System.Windows.Forms.TextBox tbH;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cbPuertos;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btConectar;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbAct;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ Practica1-Sist. Sensado y Monitoreo/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+namespace Practica1_Sist.Sensado_y_Monitoreo
+{
+    public partial class Form1 : Form
+    {
+        SerialPort puertos;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.puertos = new SerialPort();
+            puertos.DataReceived += Puertos_DataReceived;
+        }
+        private void Puertos_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                CheckForIllegalCrossThreadCalls = false;
+                SerialPort DatosSerial = (SerialPort)sender;
+                tbDatos.Text = DatosSerial.ReadLine();
+                String[] Cadena;
+                Cadena = tbDatos.Text.Split('/');
+                tbDist.Text = Cadena[3];
+                tbTemp.Text = Cadena[1];
+                tbH.Text = Cadena[2];
+                tbAct.Text = Cadena[4];
+            }
+            catch { }           
+        }
+        private void cmbPuertos_DropDown(object sender, EventArgs e)
+        {
+            cbPuertos.Items.Clear();
+            String[] PuertosDisponibles = SerialPort.GetPortNames();
+            foreach (string puerto in PuertosDisponibles)
+            {
+                cbPuertos.Items.Clear();
+                cbPuertos.Items.Add(puerto);
+            }
+        }
+        private void btConectar_Click(object sender, EventArgs e)
+        {
+            if(btConectar.Text=="Conectar")
+            {
+                puertos.BaudRate = 9600;
+                try
+                {
+                    puertos.PortName = cbPuertos.SelectedItem.ToString();
+                }
+                catch
+                {
+                    MessageBox.Show("Seleccione un puerto, por favor","Error",MessageBoxButtons.OK);
+                }
+                if (!puertos.IsOpen)
+                {
+                    try
+                    {
+                        puertos.Open();
+                        btConectar.Text = "Desconectar";
+                    }
+                    catch { }
+                }                
+            }
+            else if (btConectar.Text == "Desconectar")
+            {
+                if (puertos.IsOpen)
+                {
+                    puertos.Close();
+                    btConectar.Text = "Conectar";
+                    puertos.Dispose();
+                    tbDatos.Clear();
+                }
+            }
+        }
+    }
+}
